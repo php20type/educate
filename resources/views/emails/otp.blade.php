80531b17478625ac08b4b8c4ee8adeb566fd5e6e
Your GitHub Username <Your GitHub Email>
2024-07-16 12:49:26 +0530

    email template

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -1,1 +1,45 @@
-<p>Your One Time Password (OTP) for registration is: <strong>{{ $otp }}</strong></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Site Title -->
+    <title></title>
+
+    <!-- Character Set and Responsive Meta Tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+</head>
+
+<body style="background: #0C1018; font-family: Arial, sans-serif; margin: 0px; padding: 0px;">
+
+<table align="center" width="100%" height="100vh" cellspacing="0" cellpadding="0" style="max-width: 800px; margin: 160px auto; text-align: center;">
+    <tr>
+        <td style="padding: 20px 0;">
+            <img src="https://educate.finitycodes.com/img/logo/logo.svg" alt="">
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #ffffff; font-size: 24px; font-weight: 500; line-height: 36px; padding: 20px 0;">
+            Your One Time Password (OTP) for registration is:
+        </td>
+    </tr>
+    <tr>
+        <td style="padding: 20px 0;">
+            <span style="background: #367BF5; color: #ffffff; font-weight: bold; padding: 10px 20px; border-radius: 2px; display: inline-block; font-size: 18px;">
+                {{ $otp }}
+            </span>
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #ffffff; font-size: 16px; font-weight: 400; padding: 20px 0;">
+            If you did not request a code, you do not have to do anything.
+        </td>
+    </tr>
+    <tr>
+        <td style="color: #ffffff; font-size: 16px; font-weight: 400; padding: 20px 0;">
+            All rights reserved 2024.
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>
